8f07fece8018809219f607f46bd8c662a837883d
Felix Hoffmann <Felix.Hoffmann.10@gmx.de>
2024-04-21 16:56:23 +0200

    zahl auf display

--- taschenrechner.py
+++ taschenrechner.py
@@ -1,16 +1,5 @@
 import tkinter as tk
 
-class Rechner:
-    
-    def __init__(self):
-        self.val = 0
-
-    def summe(self, a, b):
-        self.val = a + b
-    
-    def differenz(self, a, b):
-        self.val = a - b
-    
 class Settings:
     def __init__(self):
         self.size = 10
@@ -21,19 +10,42 @@ class Settings:
 def main(root):
     settings = Settings()
 
-    erg = tk.Label(root, height=settings.size, background='lightblue', text='ERGEBNIS')
-    erg.pack(fill=tk.X)
+    erg_zeile = tk.Label(root, height=settings.size, background='lightblue', text='0')
+    erg_zeile.pack(fill=tk.X)
     eingabe = tk.Frame(root)
     eingabe.pack()
 
     buttons = [tk.Button(eingabe, width=settings.size * 2, height=settings.size, text=settings.text[i], background='lightgray')
                for i in range(settings.rows * settings.columns)]
 
+    for b in buttons:
+        b['command'] = lambda b=b : fun(b)
+
     for row in range(settings.rows):
         for column in range(settings.columns):
             i = row * settings.columns + column
             buttons[i].grid(row=row, column=column)
-            print(i)
+
+    plus = False
+    minus = False
+    lösung = False
+    buffer = 0
+
+    def fun(button):
+        x = button['text']
+        if x == '+':
+            plus = True
+            buffer = erg_zeile['text']
+        elif x == '-':
+            minus = True
+            buffer = erg_zeile['text']
+        elif x == '=':
+            pass     
+        else:
+            display_number(button['text'])
+
+    def display_number(n):
+        erg_zeile['text'] = n
 
 if __name__ == '__main__':
     root = tk.Tk()
